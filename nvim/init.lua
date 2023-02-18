7016e3e9f6db5c0124db7fad5ed75bb42400ad1b
Bryan <bryanathanaelj@gmail.com>
2023-02-18 04:20:49 +0100

    Add options, keymaps, and essential plugins config

---
+++ nvim/init.lua
@@ -0,0 +1,3 @@
+require('bryan/plugins')
+require('bryan/options')
+require('bryan/keymaps')
---
+++ nvim/lua/bryan/options.lua
@@ -0,0 +1,49 @@
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.wildmode = 'longest:full,full' -- complete the longest common match, and allow tabbing the results to fully complete them
+
+vim.opt.title = true
+vim.opt.mouse = 'a' -- enable mouse for all modes
+
+vim.opt.termguicolors = true
+
+vim.opt.spell = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.list = true -- enable below listchars
+vim.opt.listchars = { tab = '▸ ', trail = '·' }
+vim.opt.fillchars:append({ eob = ' ' }) -- remove the ~ from end of buffer
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.hidden = true
+
+vim.wo.number = true
+
+vim.opt.confirm = true
+
+vim.opt.undofile = true -- persistent undo
+vim.opt.backupcopy = "yes"
