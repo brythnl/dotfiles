10074c7ae9c45a43c5fffe23c8a93767e2a1b812
Bryan <bryanathanaelj@gmail.com>
2023-02-20 17:30:56 +0100

    Configure linting and formatting plugins

--- nvim/lua/bryan/plugins/lspconfig.lua
+++ nvim/lua/bryan/plugins/lspconfig.lua
@@ -28,6 +28,27 @@ require('lspconfig').jsonls.setup({
   },
 })
 
+-- null-ls
+require('null-ls').setup({
+  sources = {
+    require('null-ls').builtins.diagnostics.eslint_d.with({
+      condition = function(utils)
+        return utils.root_has_file({ '.eslintrc.js' })
+      end,
+    }),
+    require('null-ls').builtins.diagnostics.trail_space.with({ disabled_filetypes = { 'NvimTree' } }),
+    require('null-ls').builtins.formatting.eslint_d.with({
+      condition = function(utils)
+        return utils.root_has_file({ '.eslintrc.js' })
+      end,
+    }),
+    require('null-ls').builtins.formatting.prettierd,
+  },
+})
+
+-- Has to be after calling null-ls
+require('mason-null-ls').setup({ automatic_installation = true })
+
 -- Keymaps
 vim.keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>") -- show definition, references
 vim.keymap.set({"n","v"}, "<leader>ca", "<cmd>Lspsaa code_action<CR>") -- Show all code actions
@@ -41,12 +62,15 @@ vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>") -- go to 
 -- Use <C-t> to jump back
 vim.keymap.set("n", "gt", "<cmd>Lspsaga peek_type_definition<CR>")
 vim.keymap.set("n", "gr", "<cmd>Lspsaga rename<CR>") -- Rename all occurrences of the hovered word for the entire file
-vim.keymap.set("n", "<leader>ld", "<cmd>Lspsaga show_line_diagnostics<CR>") -- show  diagnostics for line
+vim.keymap.set("n", "<leader>d", "<cmd>Lspsaga show_line_diagnostics<CR>") -- show  diagnostics for line
 vim.keymap.set("n", "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics<CR>") -- show diagnostics for cursor
 vim.keymap.set("n", "<leader>bd", "<cmd>Lspsaga show_buf_diagnostics<CR>") -- show diagnostics for buffer
 vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
 vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>')
 vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
+
+-- Commands
+vim.api.nvim_create_user_command('Format', vim.lsp.buf.formatting, {})
 
 -- Diagnostic configuration
 vim.diagnostic.config({
