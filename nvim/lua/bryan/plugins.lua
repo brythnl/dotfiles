4c6dcc9c0a2a2e76dd038ac4e4af2a9240cc22e1
Bryan <bryanathanaelj@gmail.com>
2023-02-21 19:35:39 +0100

    Change default keymap of split of splitjoin

--- nvim/lua/bryan/plugins.lua
+++ nvim/lua/bryan/plugins.lua
@@ -137,6 +137,7 @@ use({
     vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
     vim.g.splitjoin_trailing_comma = 1
     vim.g.splitjoin_php_method_chain_full = 1
+    vim.keymap.set('n', 'gl', ':SplitjoinSplit<CR>')
   end,
 })
 
