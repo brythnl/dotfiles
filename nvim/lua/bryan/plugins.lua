94882202cede7cbc5291ee294f804b7058787acc
Bryan <bryanathanaelj@gmail.com>
2023-02-23 16:07:49 +0100

    Change keymap to toggle floating terminal

--- nvim/lua/bryan/plugins.lua
+++ nvim/lua/bryan/plugins.lua
@@ -242,8 +242,8 @@ use({
   config = function()
     vim.g.floaterm_width = 0.8
     vim.g.floaterm_height = 0.8
-    vim.keymap.set('n', '<F1>', ':FloatermToggle<CR>')
-    vim.keymap.set('t', '<F1>', '<C-\\><C-n>:FloatermToggle<CR>')
+    vim.keymap.set('n', '<leader>`', ':FloatermToggle<CR>')
+    vim.keymap.set('t', '<leader>`', '<C-\\><C-n>:FloatermToggle<CR>')
     vim.cmd([[
       highlight link Floaterm CursorLine
       highlight link FloatermBorder CursorLineBg
