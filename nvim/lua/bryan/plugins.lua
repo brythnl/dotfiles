892b478e512e352879c9cd99934f8e1f1a5bb74f
Bryan <bryanathanaelj@gmail.com>
2024-02-20 00:18:56 +0100

    migrate to lazy

--- nvim/lua/bryan/plugins.lua
+++ nvim/lua/bryan/plugins.lua
@@ -1,56 +1,31 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
-require('packer').reset()
-require('packer').init({
-  compile_path = vim.fn.stdpath('data')..'/site/plugin/packer_compiled.lua',
-  display = {
-    open_fn = function()
-      return require('packer.util').float({ border = 'solid' })
-    end,
-  },
-})
-
-local use = require('packer').use
-
--- Packer can manage itself.
-use ('wbthomason/packer.nvim')
-
---- Theme
-use({
+require('lazy').setup({
+  --- Theme
+  {
     'rose-pine/neovim',
-    as = 'rose-pine',
-    -- 'olivercederborg/poimandres.nvim',
-    -- 'aktersnurra/no-clown-fiesta.nvim',
-    -- 'craftzdog/solarized-osaka.nvim',
-    -- 'rebelot/kanagawa.nvim',
+    name = 'rose-pine',
+    dependencies = 'akinsho/bufferline.nvim',
     config = function()
-      -- require('poimandres').setup({
-        -- disable_background = true,
-      -- })
       require('rose-pine').setup({
         variant = 'auto',
         disable_background = true,
         disable_italics = true
       })
-      -- require('no-clown-fiesta').setup({
-      -- })
-      -- require('solarized-osaka').setup({
-      --   transparent = false
-      -- })
-      -- require('kanagawa').setup({
-      --   transparent = false
-      -- })
 
       vim.cmd('colorscheme rose-pine')
 
@@ -74,319 +49,283 @@ use({
 
           vim.api.nvim_set_hl(0, 'IndentBlanklineChar', { fg = '#2F313C' })
     end,
-})
-
--- Commenting Support
-use ('tpope/vim-commentary')
-
--- Add, change, and delete surrounding text.
-use('tpope/vim-surround')
-
--- Useful commands like :Rename and :SudoWrite.
-use('tpope/vim-eunuch')
-
--- Indent autodetection with editorconfig support.
-use('tpope/vim-sleuth')
-
--- Allow plugins to enable repeating of commands.
-use('tpope/vim-repeat')
-
--- Add more languages.
-use('sheerun/vim-polyglot')
-
--- Navigate seamlessly between Vim windows and Tmux panes.
-use('christoomey/vim-tmux-navigator')
-
--- Jump to the last location when opening a file.
-use('farmergreg/vim-lastplace')
-
--- Enable * searching with visually selected text.
-use('nelstrom/vim-visual-star-search')
-
--- Automatically create parent dirs when saving.
-use('jessarcher/vim-heritage')
-
--- Text objects for HTML attributes.
-use({
-  'whatyouhide/vim-textobj-xmlattr',
-  requires = 'kana/vim-textobj-user',
-})
-
--- Automatically set the working directory to the project root.
-use({
-  'airblade/vim-rooter',
-  setup = function()
-    -- Instead of this running every time we open a file, we'll just run it once when Vim starts.
-    vim.g.rooter_manual_only = 1
-  end,
-  config = function()
-    vim.cmd('Rooter')
-  end,
-})
-
--- Automatically add closing brackets, quotes, etc.
-use({
-  'windwp/nvim-autopairs',
-  config = function()
-    require('nvim-autopairs').setup()
-  end,
-})
-
--- Automatically close html tags
-use ({
-  'windwp/nvim-ts-autotag',
-  config = function()
-    require('nvim-ts-autotag').setup()
-  end,
-})
-
--- Add smooth scrolling to avoid jarring jumps
-use({
-  'karb94/neoscroll.nvim',
-  config = function()
-    require('neoscroll').setup()
-  end,
-})
-
--- All closing buffers without closing the split window.
-use({
-  'famiu/bufdelete.nvim',
-  config = function()
-    vim.keymap.set('n', '<Leader>q', ':Bdelete<CR>')
-  end,
-})
-
--- Split arrays and methods onto multiple lines, or join them back up.
-use({
-  'AndrewRadev/splitjoin.vim',
-  config = function()
-    vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
-    vim.g.splitjoin_trailing_comma = 1
-    vim.g.splitjoin_php_method_chain_full = 1
-    vim.keymap.set('n', 'gl', ':SplitjoinSplit<CR>')
-  end,
-})
-
--- Automatically fix indentation when pasting code.
-use({
-  'sickill/vim-pasta',
-  config = function()
-    vim.g.pasta_disabled_filetypes = { 'fugitive' }
-  end,
-})
-
--- Fuzzy finder
-use({
-  'nvim-telescope/telescope.nvim',
-  requires = {
-    'nvim-lua/plenary.nvim',
-    'kyazdani42/nvim-web-devicons',
-    'nvim-telescope/telescope-live-grep-args.nvim',
-    { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
   },
-  config = function()
-    require('bryan/plugins/telescope')
-  end,
-})
 
--- File tree sidebar
-use({
-  'kyazdani42/nvim-tree.lua',
-  requires = 'kyazdani42/nvim-web-devicons',
-  config = function()
-    require('bryan/plugins/nvim-tree')
-  end,
-})
+  -- Commenting Support
+  'tpope/vim-commentary',
 
--- A Status line.
-use({
-  'nvim-lualine/lualine.nvim',
-  requires = 'kyazdani42/nvim-web-devicons',
-  config = function()
-    require('bryan/plugins/lualine')
-  end,
-})
+  -- Add, change, and delete surrounding text.
+  'tpope/vim-surround',
 
--- Display buffers as tabs.
-use({
-  'akinsho/bufferline.nvim',
-  requires = 'kyazdani42/nvim-web-devicons',
-  -- after = 'poimandres.nvim',
-  after = 'rose-pine',
-  -- after = 'no-clown-fiesta.nvim',
-  -- after = 'solarized-osaka.nvim',
-  -- after = 'kanagawa.nvim',
-  config = function()
-    require('bryan/plugins/bufferline')
-  end,
-})
+  -- Useful commands like :Rename and :SudoWrite.
+  'tpope/vim-eunuch',
 
-use({
-  'lukas-reineke/indent-blankline.nvim',
-  config = function()
-    require('bryan/plugins/indent-blankline')
-  end,
-})
+  -- Indent autodetection with editorconfig support.
+  'tpope/vim-sleuth',
 
-use ({
-  'glepnir/dashboard-nvim',
-  event = 'VimEnter',
-  config = function()
-    require('bryan/plugins/dashboard-nvim')
-  end,
-  requires = {'nvim-tree/nvim-web-devicons'}
-})
+  -- Allow plugins to enable repeating of commands.
+  'tpope/vim-repeat',
 
--- Git integration.
-use({
-  'lewis6991/gitsigns.nvim',
-  config = function()
-    require('gitsigns').setup()
-    vim.keymap.set('n', ']h', ':Gitsigns next_hunk<CR>')
-    vim.keymap.set('n', '[h', ':Gitsigns prev_hunk<CR>')
-    vim.keymap.set('n', 'gs', ':Gitsigns stage_hunk<CR>')
-    vim.keymap.set('n', 'gS', ':Gitsigns undo_stage_hunk<CR>')
-    vim.keymap.set('n', 'gp', ':Gitsigns preview_hunk<CR>')
-    vim.keymap.set('n', 'gb', ':Gitsigns blame_line<CR>')
-  end,
-})
+  -- Navigate seamlessly between Vim windows and Tmux panes.
+  'christoomey/vim-tmux-navigator',
 
+  -- Jump to the last location when opening a file.
+  'farmergreg/vim-lastplace',
 
--- Git commands.
-use({
-  'tpope/vim-fugitive',
-  requires = 'tpope/vim-rhubarb',
-})
+  -- Enable * searching with visually selected text.
+  'nelstrom/vim-visual-star-search',
 
--- Floating terminal.
-use({
-  'voldikss/vim-floaterm',
-  config = function()
-    vim.g.floaterm_width = 0.8
-    vim.g.floaterm_height = 0.8
-    vim.keymap.set('n', '<leader>`', ':FloatermToggle<CR>')
-    vim.keymap.set('t', '<leader>`', '<C-\\><C-n>:FloatermToggle<CR>')
-    vim.cmd([[
-      highlight link Floaterm CursorLine
-      highlight link FloatermBorder CursorLineBg
-    ]])
-  end
-})
+  -- Automatically create parent dirs when saving.
+  'jessarcher/vim-heritage',
 
--- Emmet
-use ({
-  "mattn/emmet-vim",
-  setup = function () -- load stuff before the plugin is loaded
-    require('bryan/plugins/emmet')
-  end,
-})
-
--- Improved syntax highlighting
-use({
-  'nvim-treesitter/nvim-treesitter',
-  run = function()
-    require('nvim-treesitter.install').update({ with_sync = true })
-  end,
-  requires = {
-    'JoosepAlviste/nvim-ts-context-commentstring',
-    'nvim-treesitter/nvim-treesitter-textobjects',
+  -- Text objects for HTML attributes.
+  {
+    'whatyouhide/vim-textobj-xmlattr',
+    dependencies = 'kana/vim-textobj-user',
   },
-  config = function()
-    require('bryan/plugins/treesitter')
-  end,
-})
 
--- Language Server Protocol.
-use({
-  'neovim/nvim-lspconfig',
-  requires = {
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-    'b0o/schemastore.nvim',
-    'jose-elias-alvarez/null-ls.nvim',
-    'jayp0521/mason-null-ls.nvim',
-  },
-  config = function()
-    require('bryan/plugins/lspconfig')
-  end,
-})
-
-use({
-    "glepnir/lspsaga.nvim",
-    branch = "main",
-    config = function()
-        require("lspsaga").setup({
-            move_in_saga = { prev = "<C-k>", next = "<C-j>" },
-            finder_action_keys = {
-              open = "<CR>",
-              },
-              definition_action_keys = {
-                  edit = "<CR>",
-              },
-        })
+  -- Automatically set the working directory to the project root.
+  {
+    'airblade/vim-rooter',
+    init = function()
+      -- Instead of this running every time we open a file, we'll just run it once when Vim starts.
+      vim.g.rooter_manual_only = 1
     end,
-    requires = {
-        {"nvim-tree/nvim-web-devicons"},
-        --Please make sure you install markdown and markdown_inline parser
-        {"nvim-treesitter/nvim-treesitter"}
-    },
-})
-
--- Completion
-use({
-  'hrsh7th/nvim-cmp',
-  requires = {
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-nvim-lsp-signature-help',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'L3MON4D3/LuaSnip',
-    'saadparwaiz1/cmp_luasnip',
-    'onsails/lspkind-nvim',
+    config = function()
+      vim.cmd('Rooter')
+    end,
   },
-  config = function()
-    require('bryan/plugins/cmp')
-  end,
+
+  -- Automatically add closing brackets, quotes, etc.
+  {
+    'windwp/nvim-autopairs',
+    config = function()
+      require('nvim-autopairs').setup()
+    end,
+  },
+
+  -- Automatically close html tags
+  {
+    'windwp/nvim-ts-autotag',
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end,
+  },
+
+  -- Add smooth scrolling to avoid jarring jumps
+  {
+    'karb94/neoscroll.nvim',
+    config = function()
+      require('neoscroll').setup()
+    end,
+  },
+
+  -- All closing buffers without closing the split window.
+  {
+    'famiu/bufdelete.nvim',
+    config = function()
+      vim.keymap.set('n', '<Leader>q', ':Bdelete<CR>')
+    end,
+  },
+
+  -- Split arrays and methods onto multiple lines, or join them back up.
+  {
+    'AndrewRadev/splitjoin.vim',
+    config = function()
+      vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
+      vim.g.splitjoin_trailing_comma = 1
+      vim.g.splitjoin_php_method_chain_full = 1
+      vim.keymap.set('n', 'gl', ':SplitjoinSplit<CR>')
+    end,
+  },
+
+  -- Automatically fix indentation when pasting code.
+  {
+    'sickill/vim-pasta',
+    config = function()
+      vim.g.pasta_disabled_filetypes = { 'fugitive' }
+    end,
+  },
+
+  -- Fuzzy finder
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'nvim-telescope/telescope-live-grep-args.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    config = function()
+      require('bryan/plugins/telescope')
+    end,
+  },
+
+  -- File tree sidebar
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      require('bryan/plugins/nvim-tree')
+    end,
+  },
+
+  -- A Status line.
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      require('bryan/plugins/lualine')
+    end,
+  },
+
+  -- Display buffers as tabs.
+  {
+    'akinsho/bufferline.nvim',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      require('bryan/plugins/bufferline')
+    end,
+  },
+
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    config = function()
+      require('bryan/plugins/indent-blankline')
+    end,
+  },
+
+  {
+    'glepnir/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require('bryan/plugins/dashboard-nvim')
+    end,
+    dependencies = {'nvim-tree/nvim-web-devicons'}
+  },
+
+  -- Git integration.
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+      vim.keymap.set('n', ']h', ':Gitsigns next_hunk<CR>')
+      vim.keymap.set('n', '[h', ':Gitsigns prev_hunk<CR>')
+      vim.keymap.set('n', 'gs', ':Gitsigns stage_hunk<CR>')
+      vim.keymap.set('n', 'gS', ':Gitsigns undo_stage_hunk<CR>')
+      vim.keymap.set('n', 'gp', ':Gitsigns preview_hunk<CR>')
+      vim.keymap.set('n', 'gb', ':Gitsigns blame_line<CR>')
+    end,
+  },
+
+  -- Git commands.
+  {
+    'tpope/vim-fugitive',
+    dependencies = 'tpope/vim-rhubarb',
+  },
+
+  -- Emmet
+  {
+    "mattn/emmet-vim",
+    init = function () -- load stuff before the plugin is loaded
+      require('bryan/plugins/emmet')
+    end,
+  },
+
+  -- Improved syntax highlighting
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = function()
+      require('nvim-treesitter.install').update({ with_sync = true })
+    end,
+    dependencies = {
+      'JoosepAlviste/nvim-ts-context-commentstring',
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    config = function()
+      require('bryan/plugins/treesitter')
+    end,
+  },
+
+  -- Language Server Protocol.
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+      'b0o/schemastore.nvim',
+      'jose-elias-alvarez/null-ls.nvim',
+      'jayp0521/mason-null-ls.nvim',
+    },
+    config = function()
+      require('bryan/plugins/lspconfig')
+    end,
+  },
+
+  {
+      "glepnir/lspsaga.nvim",
+      branch = "main",
+      config = function()
+          require("lspsaga").setup({
+              move_in_saga = { prev = "<C-k>", next = "<C-j>" },
+              finder_action_keys = {
+                open = "<CR>",
+                },
+                definition_action_keys = {
+                    edit = "<CR>",
+                },
+          })
+      end,
+      dependencies = {
+          {"nvim-tree/nvim-web-devicons"},
+          --Please make sure you install markdown and markdown_inline parser
+          {"nvim-treesitter/nvim-treesitter"}
+      },
+  },
+
+  -- Completion
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+      'onsails/lspkind-nvim',
+    },
+    config = function()
+      require('bryan/plugins/cmp')
+    end,
+  },
+
+
+  -- Debugger
+  'mfussenegger/nvim-dap',
+
+  {
+    "microsoft/vscode-js-debug",
+    lazy = true,
+    build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out"
+  },
+
+  {
+    "mxsdev/nvim-dap-vscode-js",
+    dependencies = {"mfussenegger/nvim-dap"},
+    config = function()
+      require('bryan/plugins/nvim-dap')
+    end,
+  },
+
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {"mfussenegger/nvim-dap"},
+    config = function()
+      require('dapui').setup()
+      vim.keymap.set('n', '<leader>ui', require('dapui').toggle)
+    end,
+  },
+
+  'theHamsta/nvim-dap-virtual-text',
 })
-
-
--- Debugger
-use 'mfussenegger/nvim-dap'
-
-use {
-  "microsoft/vscode-js-debug",
-  opt = true,
-  run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out"
-}
-
-use {
-  "mxsdev/nvim-dap-vscode-js",
-  requires = {"mfussenegger/nvim-dap"},
-  config = function()
-    require('bryan/plugins/nvim-dap')
-  end,
-}
-
-use {
-  "rcarriga/nvim-dap-ui",
-  requires = {"mfussenegger/nvim-dap"},
-  config = function()
-    require('dapui').setup()
-    vim.keymap.set('n', '<leader>ui', require('dapui').toggle)
-  end,
-}
-
-use 'theHamsta/nvim-dap-virtual-text'
-
--- Automatically set up your configuration after cloning packer.nvim
--- Put this at the end after all plugins
-if packer_bootstrap then
-    require('packer').sync()
-end
-
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile>
-  augroup end
-]])
