8afc769e4a4e283c1290464af9ccea50915145ff
bryan <bryanathanaelj@gmail.com>
2026-02-07 23:13:14 +0100

    add ai plugins for nvim

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -51,53 +51,81 @@ return {
   },
 
   -- AI stuff
-  -- {
-  --   "olimorris/codecompanion.nvim",
-  --   lazy = false,
-  --   version = "^18.0.0",
-  --   opts = {
-  --     interactions = {
-  --       chat = {
-  --         adapter = "gemini_cli"
-  --       },
-  --     },
-  --   },
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     "nvim-treesitter/nvim-treesitter",
-  --     "ravitemer/mcphub.nvim",
-  --   },
-  --   keys = {
-  --     { "<leader>cc", "<cmd>CodeCompanionChat<cr>" },
-  --   },
-  -- },
-  --
-  -- {
-  --   "saghen/blink.cmp",
-  --   opts = {
-  --     sources = {
-  --       per_filetype = {
-  --         codecompanion = { "codecompanion" },
-  --       }
-  --     },
-  --   },
-  -- },
-  --
-  -- {
-  -- "MeanderingProgrammer/render-markdown.nvim",
-  --   ft = { "markdown", "codecompanion" }
-  -- },
-  --
-  -- {
-  --   "HakonHarnes/img-clip.nvim",
-  --   opts = {
-  --     filetypes = {
-  --       codecompanion = {
-  --         prompt_for_file_name = false,
-  --         template = "[Image]($FILE_PATH)",
-  --         use_absolute_path = true,
-  --       },
-  --     },
-  --   },
-  -- },
+  {
+    "olimorris/codecompanion.nvim",
+    lazy = false,
+    version = "^18.0.0",
+    opts = {
+      interactions = {
+        chat = {
+          adapter = {
+            name = "gemini",
+            model = "gemini-3-flash-preview"
+          },
+        },
+      },
+      adapter = {
+        gemini = function()
+          return require("codecompanion.adapters").extend("gemini", {
+            env = {
+              api_key = "GEMINI_API_KEY",
+            },
+          })
+        end,
+      },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "ravitemer/mcphub.nvim",
+    },
+    keys = {
+      { "<leader>ct", "<cmd>CodeCompanionChat Toggle<cr>" },
+    },
+  },
+
+  {
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        per_filetype = {
+          codecompanion = { "codecompanion" },
+        }
+      },
+    },
+  },
+
+   {
+   "MeanderingProgrammer/render-markdown.nvim",
+     ft = { "markdown", "codecompanion" }
+   },
+
+   {
+     "HakonHarnes/img-clip.nvim",
+     opts = {
+       filetypes = {
+         codecompanion = {
+           prompt_for_file_name = false,
+           template = "[Image]($FILE_PATH)",
+           use_absolute_path = true,
+         },
+       },
+     },
+   },
+
+  {
+    "marcinjahn/gemini-cli.nvim",
+    cmd = "Gemini",
+    -- Example key mappings for common actions:
+    keys = {
+      { "<leader>gt", "<cmd>Gemini toggle<cr>", desc = "Toggle Gemini CLI" },
+      { "<leader>ga", "<cmd>Gemini ask<cr>", desc = "Ask Gemini", mode = { "n", "v" } },
+      { "<leader>gf", "<cmd>Gemini add_file<cr>", desc = "Add File" },
+
+    },
+    dependencies = {
+      "folke/snacks.nvim",
+    },
+    config = true,
+  },
 }
