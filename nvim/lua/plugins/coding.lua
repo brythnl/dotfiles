036d5fa2bd998142931aa38cb08cab5aa8252502
bryan <bryanathanaelj@gmail.com>
2026-02-08 15:21:18 +0100

    update nvm config

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -50,82 +50,16 @@ return {
     }
   },
 
-  -- AI stuff
   {
-    "olimorris/codecompanion.nvim",
-    lazy = false,
-    version = "^18.0.0",
+    "mfussenegger/nvim-lint",
+    optional = true,
     opts = {
-      interactions = {
-        chat = {
-          adapter = {
-            name = "gemini",
-            model = "gemini-3-flash-preview"
-          },
+      linters = {
+        ["markdownlint-cli2"] = {
+          prepend_args = { "--config", os.getenv("HOME") .. "/.config/nvim/configs/.markdownlint-cli2.yaml", "--" },
         },
       },
-      adapter = {
-        gemini = function()
-          return require("codecompanion.adapters").extend("gemini", {
-            env = {
-              api_key = "GEMINI_API_KEY",
-            },
-          })
-        end,
-      },
     },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "ravitemer/mcphub.nvim",
-    },
-    keys = {
-      { "<leader>ct", "<cmd>CodeCompanionChat Toggle<cr>" },
-    },
-  },
-
-  {
-    "saghen/blink.cmp",
-    opts = {
-      sources = {
-        per_filetype = {
-          codecompanion = { "codecompanion" },
-        }
-      },
-    },
-  },
-
-   {
-   "MeanderingProgrammer/render-markdown.nvim",
-     ft = { "markdown", "codecompanion" }
-   },
-
-   {
-     "HakonHarnes/img-clip.nvim",
-     opts = {
-       filetypes = {
-         codecompanion = {
-           prompt_for_file_name = false,
-           template = "[Image]($FILE_PATH)",
-           use_absolute_path = true,
-         },
-       },
-     },
-   },
-
-  {
-    "marcinjahn/gemini-cli.nvim",
-    cmd = "Gemini",
-    -- Example key mappings for common actions:
-    keys = {
-      { "<leader>gt", "<cmd>Gemini toggle<cr>", desc = "Toggle Gemini CLI" },
-      { "<leader>ga", "<cmd>Gemini ask<cr>", desc = "Ask Gemini", mode = { "n", "v" } },
-      { "<leader>gf", "<cmd>Gemini add_file<cr>", desc = "Add File" },
-
-    },
-    dependencies = {
-      "folke/snacks.nvim",
-    },
-    config = true,
-  },
+  }
 }
+
